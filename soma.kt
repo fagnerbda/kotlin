3d3cd7b859bf81e4551144034d75b42c987a2ae2
fagnerbda <fagnerssanto3@gmail.com>
2022-06-09 21:00:35 -0400

    kotlin

---
+++ soma.kt
@@ -0,0 +1,13 @@
+fun soma(x:Int, y:Int):Int{
+    return x+y
+}
+fun subtracao(x:Int, y:Int):Int{
+    return x-y
+}
+
+fun main ( ){
+    println ("soma de dois numeros")
+    println (soma(4,3))
+    println ("subtracao de dois numeros")
+    println (subtracao(4,0))
+}
